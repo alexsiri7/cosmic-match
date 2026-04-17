119379dd77ac9ea6521aac906ff569c1ab0d7314
Alex <alexsiri7@gmail.com>
2026-04-17 22:18:09 +0100

    fix: remove afterEvaluate wrapper to avoid already-evaluated error
    KotlinCompile configureEach uses lazy configuration and does not need
    afterEvaluate. The prior evaluationDependsOn block causes :app to be
    evaluated before the afterEvaluate hook can be registered, triggering
    'Cannot run Project.afterEvaluate when project is already evaluated'.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -20,13 +20,11 @@ subprojects {
 }
 
 subprojects {
-    afterEvaluate {
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-            compilerOptions {
-                languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
-                apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
-                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-            }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        compilerOptions {
+            languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
+            apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_1_9)
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
         }
     }
 }
